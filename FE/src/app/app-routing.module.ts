763979079c182f6b8996af211e59964120a19f9c
DanielLieskovsky <danol147@gmail.com>
2023-03-02 11:15:26 +0100

    Routing added Welcome page generated Route "" for welcome page

---
+++ FE/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from "@angular/router";
+import {WelcomePageComponent} from "./componetnts/welcome-page/welcome-page.component";
+
+
+
+const routes: Routes = [
+  { path:"", component: WelcomePageComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- FE/src/app/app.module.ts
+++ FE/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WelcomePageComponent } from './componetnts/welcome-page/welcome-page.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomePageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
