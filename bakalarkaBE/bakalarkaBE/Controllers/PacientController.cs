4026ae12d4e8d03a5a369b1ea8db577f57daffd9
DanielLieskovsky <danol147@gmail.com>
2023-03-10 18:06:24 +0100

    -pacient with mesto and poistovna

--- bakalarkaBE/bakalarkaBE/Controllers/PacientController.cs
+++ bakalarkaBE/bakalarkaBE/Controllers/PacientController.cs
@@ -14,10 +14,42 @@ namespace bakalarkaBE.Controllers
             _dbContext = dbContext;
         }
 
+        //return pacient s mestom a poistovnou
         [HttpGet("allPacients")]
         public async Task<ActionResult<List<Pacient>>> GetAllPacients()
         {
-            return Ok(await _dbContext.Pacients.ToListAsync());
+            return Ok(await _dbContext.Pacients.Join(_dbContext.Poistovnas,
+                a => a.Idpoistovne, b => b.Idpoistovne,
+                (a, b) => new Pacient()
+                {
+                    Rodnecislo = a.Rodnecislo,
+                    Meno = a.Meno,
+                    Priezvisko = a.Priezvisko,
+                    Ulica = a.Ulica,
+                    Idpoistovne = a.Idpoistovne,
+                    Poistenyvpoistovniod = a.Poistenyvpoistovniod,
+                    Umrtie = a.Umrtie,
+                    Idmesta = a.Idmesta,
+                    Heslo = a.Heslo,
+                    Rola = a.Rola,
+                    IdpoistovneNavigation = b,
+                    IdmestaNavigation = a.IdmestaNavigation
+                }).Join(_dbContext.Mesta,
+                a => a.Idmesta, b => b.Idmesta,
+                (a, b) => new Pacient()
+                { Rodnecislo = a.Rodnecislo,
+                    Meno = a.Meno,
+                    Priezvisko = a.Priezvisko,
+                    Ulica = a.Ulica,
+                    Idpoistovne = a.Idpoistovne,
+                    Poistenyvpoistovniod = a.Poistenyvpoistovniod,
+                    Umrtie = a.Umrtie,
+                    Idmesta = a.Idmesta,
+                    Heslo = a.Heslo,
+                    Rola = a.Rola,
+                    IdpoistovneNavigation = a.IdpoistovneNavigation,
+                    IdmestaNavigation = b
+                }).ToListAsync());
         }
     }
 }
--- bakalarkaBE/bakalarkaBE/Models/Pacient.cs
+++ bakalarkaBE/bakalarkaBE/Models/Pacient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace bakalarkaBE.Models;
 
@@ -25,25 +26,26 @@ public partial class Pacient
 
     public string Rola { get; set; } = null!;
 
+    [JsonIgnore]
     public virtual ICollection<Historiadoktorov> Historiadoktorovs { get; } = new List<Historiadoktorov>();
-
+    [JsonIgnore]
     public virtual ICollection<Historiapoistovni> Historiapoistovnis { get; } = new List<Historiapoistovni>();
-
+    [JsonIgnore]
     public virtual ICollection<Hospitalizacium> Hospitalizacia { get; } = new List<Hospitalizacium>();
 
     public virtual Mestum? IdmestaNavigation { get; set; }
 
     public virtual Poistovna? IdpoistovneNavigation { get; set; }
-
+    [JsonIgnore]
     public virtual ICollection<Odporucacilistok> Odporucacilistoks { get; } = new List<Odporucacilistok>();
-
+    [JsonIgnore]
     public virtual ICollection<PacientAlergie> PacientAlergies { get; } = new List<PacientAlergie>();
-
+    [JsonIgnore]
     public virtual ICollection<Pacientovelieky> Pacientoveliekies { get; } = new List<Pacientovelieky>();
-
+    [JsonIgnore]
     public virtual ICollection<Pacientoveochorenium> Pacientoveochorenia { get; } = new List<Pacientoveochorenium>();
-
+    [JsonIgnore]
     public virtual ICollection<Zaznam> Zaznams { get; } = new List<Zaznam>();
-
+    [JsonIgnore]
     public virtual ICollection<Doktor> Osobnecislos { get; } = new List<Doktor>();
 }
