511b6bb6cccc6bc654ed11876a87ecbab79730d7
DanielLieskovsky <danol147@gmail.com>
2023-03-11 12:48:33 +0100

    -pacienti aj bez poistovne alebo bydliska

--- bakalarkaBE/bakalarkaBE/Controllers/PacientController.cs
+++ bakalarkaBE/bakalarkaBE/Controllers/PacientController.cs
@@ -13,14 +13,14 @@ namespace bakalarkaBE.Controllers
         {
             _dbContext = dbContext;
         }
-
-        //return pacient s mestom a poistovnou
-        [HttpGet("allPacients")]
+        
+        //get pacienti aj bez poistovne alebo bydliska
+        [HttpGet("pacienti")]
         public async Task<ActionResult<List<Pacient>>> GetAllPacients()
         {
-            return Ok(await _dbContext.Pacients.Join(_dbContext.Poistovnas,
+            var pacienti = await _dbContext.Pacients.GroupJoin(_dbContext.Poistovnas,
                 a => a.Idpoistovne, b => b.Idpoistovne,
-                (a, b) => new Pacient()
+                (a, b) => new
                 {
                     Rodnecislo = a.Rodnecislo,
                     Meno = a.Meno,
@@ -34,22 +34,9 @@ namespace bakalarkaBE.Controllers
                     Rola = a.Rola,
                     IdpoistovneNavigation = b,
                     IdmestaNavigation = a.IdmestaNavigation
-                }).Join(_dbContext.Mesta,
-                a => a.Idmesta, b => b.Idmesta,
-                (a, b) => new Pacient()
-                { Rodnecislo = a.Rodnecislo,
-                    Meno = a.Meno,
-                    Priezvisko = a.Priezvisko,
-                    Ulica = a.Ulica,
-                    Idpoistovne = a.Idpoistovne,
-                    Poistenyvpoistovniod = a.Poistenyvpoistovniod,
-                    Umrtie = a.Umrtie,
-                    Idmesta = a.Idmesta,
-                    Heslo = a.Heslo,
-                    Rola = a.Rola,
-                    IdpoistovneNavigation = a.IdpoistovneNavigation,
-                    IdmestaNavigation = b
-                }).ToListAsync());
+                }).ToListAsync();
+
+            return Ok(pacienti);
         }
     }
 }
